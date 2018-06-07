0f83fad76f831cb8e9614466fc5c851f13533fa2
wanghq92 <835786510@qq.com>
2018-06-07 16:15:56 +0800

    first_commit

---
+++ src/com/liuw/controller/Test.java
@@ -0,0 +1,19 @@
+package com.liuw.controller;
+
+public class Test{
+	
+	public static void main(String[] args){
+		
+		final String s="HH";
+		int a=s.length();
+		//Long valueOf = Long.valueOf(null);
+		//Long parseLong = Long.parseLong("");
+		//System.out.println(valueOf);
+		
+		Long l = new Long("4");
+		System.out.println(l);
+		
+	}
+}
+
+
